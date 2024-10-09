7e7f98367012760e04b98a927c0d8d9eb4030d33
Braden Heeney <bheeney97@gmail.com>
2024-10-09 18:04:50 -0500

    new files

---
+++ Module 1/BankAccount.java
@@ -0,0 +1,56 @@
+
+public class BankAccount {
+    protected String firstName;
+    protected String lastName;
+    protected int accountID;
+    protected double balance;
+
+    protected BankAccount(){
+        this.firstName = "";
+        this.lastName = "";
+        this.accountID = 0;
+        this.balance = 0;
+    }
+    public void deposit(double dep){
+        this.balance += dep;
+        System.out.println(dep+" was added to your account.");
+    }
+    public void withdrawal(double wit){
+        this.balance -= wit;
+        System.out.println(wit+" was withdrawn from your account.");
+    }
+    public String getFirstName(){
+        return this.firstName;
+    }
+    public void setFirstName(String first){
+        this.firstName=first;
+    }
+    public String getLastName(){
+        return this.lastName;
+    }
+    public void setLastName(String last){
+        this.lastName=last;
+    }
+    public int getAccountID(){
+        return this.accountID;
+    }
+    public void setAccountID(int acc){
+        this.accountID=acc;
+    }
+    public double getBalance(){
+        return this.balance;
+    }
+    public void accountSummary(){
+        System.out.println("-----Account Summary-----");
+        System.out.println("First Name: "+this.firstName);
+        System.out.println("Last Name: "+this.lastName);
+        System.out.println("Account ID:"+ this.accountID);
+        System.out.print("Current Balance: ");
+        if(this.balance < 1 && this.balance > -1){
+            System.out.print("¢"+this.balance+"\n");
+        }
+        else{
+            System.out.print("$"+this.balance+"\n");
+        }
+    }
+}
